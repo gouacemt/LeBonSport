ca4ef8c11913dc604b37a4ae48bf9421be8f108c
Gouacem <gouacemt@gmail.com>
2026-06-22 08:51:07 +0200

    Restore: connexion useAuth et supabase depuis version Anthony

--- services/supabase.ts
+++ services/supabase.ts
@@ -1,38 +1,6 @@
 import AsyncStorage from '@react-native-async-storage/async-storage'
 import { createClient } from '@supabase/supabase-js'
 import { Platform } from 'react-native'
-<<<<<<< HEAD
-
-const storage = Platform.OS === 'web'
-  ? {
-      getItem: (key: string) =>
-        typeof localStorage !== 'undefined'
-          ? Promise.resolve(localStorage.getItem(key))
-          : Promise.resolve(null),
-      setItem: (key: string, value: string) =>
-        typeof localStorage !== 'undefined'
-          ? Promise.resolve(localStorage.setItem(key, value))
-          : Promise.resolve(),
-      removeItem: (key: string) =>
-        typeof localStorage !== 'undefined'
-          ? Promise.resolve(localStorage.removeItem(key))
-          : Promise.resolve(),
-    }
-  : AsyncStorage
-
-export const supabase = createClient(
-  process.env.EXPO_PUBLIC_SUPABASE_URL!,
-  process.env.EXPO_PUBLIC_SUPABASE_KEY!,
-  {
-    auth: {
-      storage,
-      autoRefreshToken: true,
-      persistSession: true,
-      detectSessionInUrl: false,
-    },
-  }
-)
-=======
 
 const ExpoSecureStoreAdapter = {
   getItem: async (key: string) => {
@@ -76,4 +44,3 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
     detectSessionInUrl: false,
   },
 })
->>>>>>> origin/Auth_ARS
